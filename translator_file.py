55f4e658793925ce4361a31b7334594f08bbe3ef
rupankar-02 <2017rupankarmitra@gmail.com>
2024-04-11 13:05:54 +0530

    adding requrment

--- translator_file.py
+++ translator_file.py
@@ -4,9 +4,9 @@ from googletrans import Translator
 st.header('Machine Translation Demo')
 input=st.text_area('please enter the text',value='')
 option =st.selectbox(
-      'To which language you wish to translate this text to'
-      ('Malaylam','Hindi','Tamil'))
+      'To which language you wish to translate this text to?',
+      ('Malaylam','Hindi','Bengali'))
 if st.button('Translate'):
     translator=Translator()
-    translation=translator.translate(input,dest=opion)
-    st.write(translation,text)
+    translation=translator.translate(input,dest=option)
+    st.write(translation.text)
